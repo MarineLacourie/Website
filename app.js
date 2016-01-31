979fbf0462aa1215414e7da33cbb48d75b5e2d48
Marine <marine.lacourie@gmail.com>
2016-01-31 11:34:33 +0800

    added google maps

--- app.js
+++ app.js
@@ -11,6 +11,20 @@
 		this._menu.addEventListener("click", function() {
 			this._menu.classList.toggle("opened");
 		}.bind(this), false);
+
+		var mapContainer = document.getElementById('location-map');
+		var location = {lat: 43.632826, lng: -0.117326};
+	    var map = new google.maps.Map(mapContainer, {
+	      center: location,
+	      zoom: 17,
+	      mapTypeId: google.maps.MapTypeId.SATELLITE
+	    });
+
+	    var marker = new google.maps.Marker({
+			position: location,
+			map: map,
+			title: 'Maison :)'
+		});
 	};
 
 	document.addEventListener("DOMContentLoaded", function() {
