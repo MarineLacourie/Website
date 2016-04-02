1c8f04a2d843856b1f60342f235c70ae4e35fbd7
Marine <marine.lacourie@gmail.com>
2016-04-02 16:05:56 +0800

    fix email form and style on mobile

--- emailSender.php
+++ emailSender.php
@@ -3,19 +3,26 @@
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
     $saturday = $_POST['saturday'];
-    $saturdayChildren = $_POST['saturday-children'];
     $saturdayAdults = $_POST['saturday-adults'];
-    $saturdayNight = $_POST['saturday-night'];
+    $saturdayChildren = $_POST['saturday-children'];
+    $saturdayNight = $_POST['night'];
     $sunday = $_POST['sunday'];
     $sundayChildren = $_POST['sunday-children'];
     $sundayAdults = $_POST['sunday-adults'];
     $comments = $_POST['comments'];
 
     $messageToSend = "Nom : ".$name;
-    $messageToSend .="\r\nEmail : ".$email;
     $messageToSend .="\r\nLastName : ".$lastname;
+    $messageToSend .="\r\nEmail : ".$email;
     $messageToSend .="\r\nSamedi : ".$saturday;
     $messageToSend .="\r\nSamedi Adults :".$saturdayAdults;
+    $messageToSend .="\r\nSamedi Enfants :".$saturdayChildren;
+    $messageToSend .="\r\nSamedi Nuits :".$saturdayNight;
+
+    $messageToSend .="\r\nDimanche :".$sunday;
+    $messageToSend .="\r\Dimanche Adults :".$sundayAdults;
+    $messageToSend .="\r\Dimanche Enfants :".$sundayChildren;
+
     $messageToSend .="\r\nComments :".$comments;
    
     mail("marine.lacourie@gmail.com", "Mariage Registration", $messageToSend);
