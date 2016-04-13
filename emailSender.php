232463d3e7bb82f6810b02dc95ba3ddc8d89c292
Marine <marine.lacourie@gmail.com>
2016-04-13 17:37:23 +0800

    improved the form

--- emailSender.php
+++ emailSender.php
@@ -4,26 +4,30 @@
     $email = $_POST['email'];
     $saturday = $_POST['saturday'];
     $saturdayAdults = $_POST['saturday-adults'];
+    $namesSaturday = $_POST['namesSaturday'];
     $saturdayChildren = $_POST['saturday-children'];
     $saturdayNight = $_POST['night'];
     $sunday = $_POST['sunday'];
     $sundayChildren = $_POST['sunday-children'];
     $sundayAdults = $_POST['sunday-adults'];
+    $namesSundayAdults = $_POST['namesSunday'];
     $comments = $_POST['comments'];
 
-    $messageToSend = "Nom : ".$name;
-    $messageToSend .="\r\nLastName : ".$lastname;
-    $messageToSend .="\r\nEmail : ".$email;
-    $messageToSend .="\r\nSamedi : ".$saturday;
-    $messageToSend .="\r\nSamedi Adults :".$saturdayAdults;
-    $messageToSend .="\r\nSamedi Enfants :".$saturdayChildren;
-    $messageToSend .="\r\nSamedi Nuits :".$saturdayNight;
+    $messageToSend = "Prénom : ".$name;
+    $messageToSend .="\r\nNom : ".$lastname;
+    $messageToSend .="\r\nE-mail : ".$email;
+    $messageToSend .="\r\nPrésence samedi soir : ".$saturday;
+    $messageToSend .="\r\nNombre adultes samedi soir : ".$saturdayAdults;
+    $messageToSend .="\r\nPrénoms adultes samedi soir : ".$namesSaturday;
+    $messageToSend .="\r\nNombre enfants samedi soir : ".$saturdayChildren;
+    $messageToSend .="\r\nNombre tentes :".$saturdayNight;
 
-    $messageToSend .="\r\nDimanche :".$sunday;
-    $messageToSend .="\r\Dimanche Adults :".$sundayAdults;
-    $messageToSend .="\r\Dimanche Enfants :".$sundayChildren;
+    $messageToSend .="\r\nPrésence dimanche midi : ".$sunday;
+    $messageToSend .="\r\nNombre adultes dimanche midi : ".$sundayAdults;
+    $messageToSend .="\r\Prénoms adultes dimanche midi : ".$namesSunday;
+    $messageToSend .="\r\nNombre enfants dimanche midi : ".$sundayChildren;
 
-    $messageToSend .="\r\nComments :".$comments;
+    $messageToSend .="\r\nCommentaires : ".$comments;
    
     mail("marine.lacourie@gmail.com", "Mariage Registration", $messageToSend);
     header('Location: http://royer-bertrand.com/mariage/');
